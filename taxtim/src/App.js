1df6f291b452064d08c01d79d4cdc6e44d17bb42
gauravvxv <gauravxv0410@gmail.com>
2023-12-02 14:36:55 +0530

    not complete

--- taxtim/src/App.js
+++ taxtim/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import { Footer } from './Pages/Footer';
 import Navbar from './Pages/Navbar';
 import AllRoutes from './Routes/AllRoutes';
 
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{width:'100%'}}>
       <Navbar />
+      
      <AllRoutes />
+     <Footer/>
     </div>
   );
 }
